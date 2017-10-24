b7b6ac13d0bff181ac389b4136657e499e437d36
vitoyuan <vitoooyuan@gmail.com>
2017-10-25 00:21:51 +0900

    add navbar & sidebar

--- webapp/app/App.jsx
+++ webapp/app/App.jsx
@@ -8,20 +8,30 @@ class App extends React.Component {
 		
 		return (
 			<Router>
-				<div id="wrapper">
-					<nav className="navbar navbar-default navbar-static-top" role="navigation" style={{marginBottom:"0px"}}>
-						<Link to={'/'}>
-							Dashboard 1
-						</Link>
-						<br />
-						<Link to={'/dashboard2'}>
-							Dashboard 2
-						</Link>
+				<div>
+					<nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+						<div className="container-fluid">
+			        <div className="navbar-header">
+		          	<Link className="navbar-brand" to={'/'}>
+		          		Logo
+		          	</Link>
+			        </div>
+			      </div>
 					</nav>
-					<div id="page-wrapper">
-						<Route path='/' component={Dashboard} exact />
-						<Route path='/dashboard2' component={Dashboard2} />
-					</div>
+					<div className="container-fluid">
+						<div className="row">
+			        <div className="col-md-2 sidebar">
+			          <ul className="nav nav-sidebar">
+			            <li><Link to={'/'}>Dashboard 1</Link></li>
+			            <li><Link to={'/dashboard2'}>Dashboard 2</Link></li>
+								</ul>
+							</div>
+							<div className="col-md-10" main>
+								<Route path='/' component={Dashboard} exact />
+								<Route path='/dashboard2' component={Dashboard2} />
+							</div>
+						</div>
+  				</div>
 				</div>
 			</Router>
 		);
